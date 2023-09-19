713e51ed7221b23a7f546cbb1da157bf7dbec984
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-09-19 17:52:51 +0900

    01. 시간 출력
    - 컴포넌트 만들기

---
+++ src/01/ClockImage.js
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+
+const ClockImage = () => {
+
+    return (
+        <img src={logo} className="App-logo" alt="logo" />
+    );
+}
+
+export default ClockImage
---
+++ src/01/ClockTime.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const ClockTime = () => {
+
+    return (
+        <>
+            <p>
+                Hello React !!
+            </p>
+            <div>
+                현재시간 : {new Date().toLocaleTimeString()}
+            </div>
+        </>
+    );
+}
+
+export default ClockTime;
