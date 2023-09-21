4117ec9a13646500ebbc61aa3a96b3a6fe1b9a6c
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-09-21 17:55:23 +0900

    업데이트
    APP.js 및 README 업데이트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Clock from './01/Clock';
-import Lotto from './02/Lotto';
-import MyCom from './03/MyCom';
-import MyDiv from './03/MyDiv';
-import Box from './04/Box';
+// import Clock from './01/Clock';
+// import Lotto from './02/Lotto';
+// import MyCom from './03/MyCom';
+// import MyDiv from './03/MyDiv';
+// import Box from './04/Box';
+// import Frcst from './05/Frcst';
+import Taccident from './06/Taccident';
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
     // <Lotto />
     // <MyCom />
     // <MyDiv />
-    <Box/>
+    // <Box/>
+    // <Frcst />
+    <Taccident />
+    
   );
 }
 
