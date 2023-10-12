76eb22e07b0420025327cf532c3777f8951bff18
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-10-12 13:06:39 +0900

    App.js 업데이트
    - 코드 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 // import MyDiv from './03/MyDiv';
 // import Box from './04/Box';
 // import Frcst from './05/Frcst';
-import Taccident from './06/Taccident';
+// import Taccident from './06/Taccident';
+// import MyRef from './07/MyRef';
+// import Gallery from './08/Gallery';
+import Busan from './09/Busan';
 
 function App() {
   return (
@@ -15,8 +18,10 @@ function App() {
     // <MyDiv />
     // <Box/>
     // <Frcst />
-    <Taccident />
-    
+    // <Taccident />
+    // <MyRef />
+    // <Gallery />
+    <Busan />
   );
 }
 
