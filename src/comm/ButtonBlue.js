0e549d17bece12196deecbf9650b8e084f543781
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-10-24 12:55:15 +0900

    Update ButtonBlue.js
    - ButtonBlue 수정

--- src/comm/ButtonBlue.js
+++ src/comm/ButtonBlue.js
@@ -1,7 +1,7 @@
 
 const ButtonBlue = (props) => {
     return (
-        <button onClick={props.handleClick}className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 rounded">
+        <button onClick={props.handleClick}className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border-none">
             {props.caption}
         </button>
     )
