45b31ccea865405087d251ade5b2cec8ebeabab8
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-02 11:09:53 +0900

    Update tailwind.config.js
    - spin animation custom 코드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 10s linear infinite',
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
